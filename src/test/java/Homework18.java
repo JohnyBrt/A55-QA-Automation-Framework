b82b4f04ed99a362085c3430243bc359aa318b50
JohnyBrt <burtoiuionut@ymail.com>
2024-02-14 14:52:56 -0500

    homework18

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Homework18 extends BaseTest{
@@ -19,14 +20,22 @@ public class Homework18 extends BaseTest{
         Thread.sleep(2000);
         // play button
         clickPlayButton();
-        Thread.sleep(2000);
+        Thread.sleep(5000);
         // assertions
 
+        boolean soundBarIsDisplayed;
+        Assert.assertTrue(soundBarIsDisplayed());
+        Thread.sleep(10000);
+    }
+    public boolean soundBarIsDisplayed() throws InterruptedException {
+        WebElement soundBarField = driver.findElement(By.cssSelector("div.plyr__controls"));
+        return soundBarField.isDisplayed();
+    }
+  public void clickPlayButton() throws InterruptedException {
+        WebElement  playButton = driver.findElement(By.xpath("//*[@id='mainFooter']//span[@class='play']"));
+      Thread.sleep(10000);
     }
 
-  public void clickPlayButton() {
-        WebElement  playButton = driver.findElement(By.xpath("//*[@id='mainFooter']//span[@class='play']"));
-    }
 
     public void clickPlayNextSong() {
         WebElement playNextSongField = driver.findElement(By.xpath("//*[@id='mainFooter']//i[@title='Play next song']"));
