dc298145bc6688f2e5e0f8b132a7796348687a54
JohnyBrt <burtoiuionut@ymail.com>
2024-03-26 13:47:40 -0400

    homework24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,11 +4,20 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
@@ -37,17 +46,23 @@ public class BaseTest {
 
     @BeforeSuite
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+      // WebDriverManager.chromedriver().setup();
+       //WebDriverManager.firefoxdriver().setup();
+      // WebDriverManager.safaridriver().setup();
     }
 
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void lunchBrowser(String baseURL){
+    public void lunchBrowser(String baseURL) throws MalformedURLException {
         // Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+        //ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--remote-allow-origins=*");
 
-        driver = new ChromeDriver(options);
+        //driver = new ChromeDriver(options);
+        //driver = new FirefoxDriver();
+        //driver = new SafariDriver();
+        driver = pickBrowser(System.getProperty("browser"));
+        System.out.println();
         //Implicit Wait
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //Explicit Wait
@@ -57,6 +72,45 @@ public class BaseTest {
         navigateToPage(baseURL);
 
     }
+
+    public WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL ="http://192.168.1.158:4444/";
+       switch (browser){
+           case "firefox":
+               WebDriverManager.firefoxdriver().setup();
+               return driver = new FirefoxDriver();
+           case "MicrosoftEdge":
+               WebDriverManager.edgedriver().setup();
+               EdgeOptions  edgeOptions = new EdgeOptions();
+               edgeOptions.addArguments("--remote-allow-origins=*");
+               return driver = new EdgeDriver(edgeOptions);
+           case "Safari":
+               WebDriverManager.safaridriver().setup();
+               return driver = new SafariDriver();
+            case "grid-ege":
+               caps.setCapability("browserName","MicrosoftEdge");
+               return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+          case "grid-firefox":
+               caps.setCapability("browserName","firefox");
+               return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+           case "grid-chrome":
+               caps.setCapability("browserName","chrome");
+               return driver =  new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+           case "grid-safari":
+               caps.setCapability("browserName","safari");
+               return driver = new RemoteWebDriver(URI.create(gridURL).toURL(),caps);
+           default:
+               WebDriverManager.chromedriver().setup();
+               ChromeOptions chromeOptions = new ChromeOptions();
+               chromeOptions.addArguments("==remote-allow-origins=*");
+               return driver = new ChromeDriver(chromeOptions);
+
+       }
+
+
+    }
+
     @AfterMethod
     public void closeBrowser(){
         driver.quit();
